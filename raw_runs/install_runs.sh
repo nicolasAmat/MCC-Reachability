9149722b759df53cc9b9a29882da0f144c84a753
Nicolas Amat <namat@laas.fr>
2022-09-15 20:09:44 +0200

    Update install_inputs

--- raw_runs/install_runs.sh
+++ raw_runs/install_runs.sh
@@ -6,5 +6,7 @@ mkdir -p runs/
 wget --no-check-certificate --progress=dot:mega https://mcc.lip6.fr/archives/MCC-2022-raw-data.tar.gz -P runs/
 tar xvf runs/MCC-2022-raw-data.tar.gz
 rm -v runs/MCC-2022-raw-data.tar.gz
+rm -r runs/*/BK_RESULTS/CSV
+rm -r runs/*/BK_RESULTS/CONFIGURATIONS
 
 echo "DONE"
