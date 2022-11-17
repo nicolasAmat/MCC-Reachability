6176333cd31bc60ba0b2819e79b3a4f4b27c7220
Nicolas Amat <namat@laas.fr>
2022-11-17 16:35:16 +0100

    Update link from MCC website (bis)

--- get_summary.sh
+++ get_summary.sh
@@ -3,7 +3,7 @@
 # Script to download the results of the Model Checking Contest 2022
 
 
-wget --no-check-certificate --progress=dot:mega https://mcc.lip6.fr/archives/raw-result-analysis.csv.zip
+wget --no-check-certificate --progress=dot:mega https://mcc.lip6.fr/2022/archives/raw-result-analysis.csv.zip
 unzip raw-result-analysis.csv.zip
 rm -rv __MACOSX raw-result-analysis.csv.zip
 
