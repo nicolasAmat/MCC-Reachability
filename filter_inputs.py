02332ffe8cb78e047e3fe74fc2a354d676822445
Nicolas Amat <namat@laas.fr>
2022-11-17 16:19:15 +0100

    Update filter script

--- filter_inputs.py
+++ filter_inputs.py
@@ -16,36 +16,46 @@ def main():
     # Arguments parser
     parser = argparse.ArgumentParser(description='MCC oracles')
 
-    group = parser.add_mutually_exclusive_group()
 
-    group.add_argument('--cex',
+    parser.add_argument('--cex',
                        action='store_true',
                        help="Filter counterexamples")
 
-    group.add_argument('--inv',
+    parser.add_argument('--inv',
                        action='store_true',
                        help="Filter invariants")
 
+    parser.add_argument('--include',
+                        dest='path_include',
+                        type=str,
+                        help="Path to included properties (.csv format)")
+
     parser.add_argument('--exclude',
-                       action='store_true',
-                       help="Path to excluded properties (.csv format)")
+                        dest='path_exclude',
+                        type=str,
+                        help="Path to excluded properties (.csv format)")
 
     parser_results = parser.parse_args()
 
     path_inputs = "INPUTS-2022/"
     path_summary = "raw-result-analysis.csv"
-    path_initial_states = "raw_runs/initial_states.csv"
 
     df = pandas.read_csv(path_summary, usecols=[
                          "### tool", "Input", "Examination", "estimated result"])
     df.columns = ['Tool', 'Input', 'Examination', 'Verdict']
     df.query("Tool == 'smpt' and (Examination == 'ReachabilityCardinality' or Examination == 'ReachabilityFireability')", inplace=True)
 
-    if parser_results.exclude_initial_states:
-        df_initial_states = pandas.read_csv(path_initial_states)
-        df_initial_states.columns = ['Input', 'Formula']
+    if parser_results.path_include:
+        df_include = pandas.read_csv(parser_results.path_include)
+        df_include.columns = ['Input', 'Formula']
     else:    
-        df_initial_states = None
+        df_include = None
+
+    if parser_results.path_exclude:
+        df_exclude = pandas.read_csv(parser_results.path_exclude)
+        df_exclude.columns = ['Input', 'Formula']
+    else:    
+        df_exclude = None
 
     for input in set(df['Input']):
         for examination in ['ReachabilityCardinality', 'ReachabilityFireability']:
@@ -61,27 +71,31 @@ def main():
                 formula = property_xml[0].text
                 _, _, kind = property_xml[2][0].tag.rpartition('}')
 
-                if verdict == "?":
-                    if parser_results.cex or parser_results.inv:
-                        root.remove(property_xml)
-
-                elif (kind == "exists-path" and verdict == "T") or (kind == "all-paths" and verdict == "F"):
-                    if parser_results.inv:
-                        root.remove(property_xml)
-                    elif parser_results.exclude and (df_initial_states['Formula'] == formula).any():
-                        root.remove(property_xml)
-                else:
-                    if parser_results.cex:
-                        root.remove(property_xml)
-                    elif parser_results.exclude and (df_initial_states['Formula'] == formula).any():
-                        root.remove(property_xml)
+                if parser_results.path_include and not (df_include['Formula'] == formula).any():
+                    root.remove(property_xml)
+                elif parser_results.path_exclude and (df_exclude['Formula'] == formula).any():
+                    root.remove(property_xml)
+                elif verdict == "?" and (parser_results.cex or parser_results.inv):
+                    root.remove(property_xml)
+                elif parser_results.inv and ((kind == "exists-path" and verdict == "T") or (kind == "all-paths" and verdict == "F")):
+                    root.remove(property_xml)
+                elif parser_results.cex and ((kind == "exists-path" and verdict == "F") or (kind == "all-paths" and verdict == "T")):
+                    root.remove(property_xml)
 
             if parser_results.cex:
                 tree.write('{}/{}/{}_CEX.xml'.format(path_inputs, input,
                            examination), encoding="utf-8", xml_declaration=True)
 
-            if parser_results.inv:
+            elif parser_results.inv:
                 tree.write('{}/{}/{}_INV.xml'.format(path_inputs, input,
+                           examination), encoding="utf-8", xml_declaration=True)
+
+            elif parser_results.path_include:
+                tree.write('{}/{}/{}_INCLUDED.xml'.format(path_inputs, input,
+                           examination), encoding="utf-8", xml_declaration=True)
+
+            elif parser_results.path_excluded:
+                tree.write('{}/{}/{}_EXCLUDED.xml'.format(path_inputs, input,
                            examination), encoding="utf-8", xml_declaration=True)
 
 
